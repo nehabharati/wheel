0b6b1d4664a0fcfc47705f7efb3cf1455eadba43
Neha Bharati <neha.bharati@bigbinary.com>
2022-01-12 15:55:28 +0530

    Added sidebar toggle

--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-// import { Search } from "@bigbinary/neeto-icons";
-// import { Button, Input } from "neetoui/v2";
 import { Container } from "neetoui/v2/layouts";
 
 import {
@@ -12,23 +10,34 @@ import {
 } from "./constants";
 import DeleteAlert from "./DeleteAlert";
 import NotesList from "./NotesList";
-// import NewNotePane from "./Pane/CreateNote";
 
 import Header from "../Common/CommonHeader";
 import Sidebar from "../Common/CommonSidebar";
 
 const Notes = () => {
-  // const [loading, setLoading] = useState(true);
-  // const [showNewNotePane, setShowNewNotePane] = useState(false);
   const [isDeleteAlertOpen, setIsDeleteAlertOpen] = useState(false);
+  const [isNoteSidebarOpen, setIsNoteSidebarOpen] = useState(true);
 
   const handleDeleteAlert = () => setIsDeleteAlertOpen(true);
+  const handleSidebarOpen = () => setIsNoteSidebarOpen(!isNoteSidebarOpen);
 
   return (
     <div className="flex">
-      <Sidebar title="Notes" labels={labels} segments={segments} tags={tags} />
+      {isNoteSidebarOpen && (
+        <Sidebar
+          title="Notes"
+          labels={labels}
+          segments={segments}
+          tags={tags}
+        />
+      )}
       <Container>
-        <Header title="All Notes" buttonText="Add Note" isNote={true} />
+        <Header
+          title="All Notes"
+          buttonText="Add Note"
+          isNote={true}
+          handleSidebarOpen={handleSidebarOpen}
+        />
 
         <NotesList
           notes={notes}
